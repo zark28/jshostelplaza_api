f503ec5b8a2625ebd0243cde61a40f6b0db23d46
zark28 <adamsabdulrazak28@gmail.com>
2023-01-12 08:01:27 +0000

    update db connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 // const MONGODB_URI = 'mongodb://127.0.0.1:27017/jshostel';
+const MONGODB_URI="mongodb+srv://jshostel101:jshostel101@cluster0.ycwzi.mongodb.net/?retryWrites=true"
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
